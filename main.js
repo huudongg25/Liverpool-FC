14c48a10aaeabd1957c2bd2bd442f98b3e397f1d
huudongg25 <thdong98225@gmail.com>
2022-04-29 22:45:57 +0700

    update file js

---
+++ main.js
@@ -0,0 +1,38 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const modal = $('.modal')
+const loginInput = $('#loginInput')
+const linkInput = $('#loginInput a')
+const modalContainer = $('.modal_container')
+
+// ===login======
+loginInput.addEventListener('click',function(){
+    modal.style.display ='flex'
+})
+
+modal.onclick = function() {
+    modal.style.display ='none'
+}
+
+modalContainer.onclick = function(e) {
+    e.stopPropagation()
+}
+
+
+linkInput.onclick = function(e) {
+    e.preventDefault()
+}
+
+
+// =====login mobile========
+
+const mobileLogin = $('#nav_item-mobile')
+const linkInputMobile = $('#nav_item-mobile a')
+
+mobileLogin.addEventListener('click',function(){
+    modal.style.display ='flex'
+})
+
+linkInputMobile.onclick = function(e) {
+    e.preventDefault()
+}
