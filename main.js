4dc03f1cd8f036b120fe01e0403a3b6e8da1e1a6
huudongg25 <thdong98225@gmail.com>
2022-04-30 09:09:32 +0700

    update responsive

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-const modal = $('.modal')
-const loginInput = $('#loginInput')
-const linkInput = $('#loginInput a')
-const modalContainer = $('.modal_container')
-const closeBtn = $('.modal-close')
+const modal = $(".modal");
+const loginInput = $("#loginInput");
+const linkInput = $("#loginInput a");
+const modalContainer = $(".modal_container");
+const closeBtn = $(".modal-close");
 
 // ===login======
-loginInput.addEventListener('click',function(){
-    modal.style.display ='flex'
-})
+loginInput.addEventListener("click", function () {
+  modal.style.display = "flex";
+});
 
-modal.onclick = function() {
-    modal.style.display ='none'
-}
+modal.onclick = function () {
+  modal.style.display = "none";
+};
 
-closeBtn.onclick = function() {
-    modal.style.display ='none'
-}
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
 
-modalContainer.onclick = function(e) {
-    e.stopPropagation()
-}
+modalContainer.onclick = function (e) {
+  e.stopPropagation();
+};
 
-
-linkInput.onclick = function(e) {
-    e.preventDefault()
-}
-
+linkInput.onclick = function (e) {
+  e.preventDefault();
+};
 
 // =====login mobile========
 
-const mobileLogin = $('#nav_item-mobile')
-const linkInputMobile = $('#nav_item-mobile a')
+const mobileLogin = $("#nav_item-mobile");
+const linkInputMobile = $("#nav_item-mobile a");
 
-mobileLogin.addEventListener('click',function(){
-    modal.style.display ='flex'
-})
+mobileLogin.addEventListener("click", function () {
+  modal.style.display = "flex";
+});
 
-linkInputMobile.onclick = function(e) {
-    e.preventDefault()
-}
+linkInputMobile.onclick = function (e) {
+  e.preventDefault();
+};
+
